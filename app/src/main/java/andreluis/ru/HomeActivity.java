420bf335667319a7da2a4fc2455e8f66696cfcc5
AndreLuis <andre_luisferreira@hotmail.com>
2016-09-29 21:13:24 -0300

    Atualizado

--- app/src/main/java/andreluis/ru/HomeActivity.java
+++ app/src/main/java/andreluis/ru/HomeActivity.java
@@ -1,16 +1,20 @@
 package andreluis.ru;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class HomeActivity extends FragmentActivity {
 
     protected Button mDesjejumButton, mAlmocoButton, mJantarButton;
+    protected ImageButton mInfoButton, mCardapioButton, mConfigButton, mCredButton;
 
     Fragment df = new DesjejumFragment();         //Criação de objetos para troca de fragments
     Fragment af = new AlmocoFragment();
@@ -21,9 +25,62 @@ public class HomeActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+
+        Context contexto = getApplicationContext(); //(??) --Necessario para funcionamento do toast
+
+
+        String texto_toast = "Este é o cardápio";                           //Toast caso o mCardapioButton seja clicado
+        int duracao = Toast.LENGTH_LONG;
+        final Toast toast = Toast.makeText(contexto, texto_toast, duracao);
+
+
         mDesjejumButton = (Button) findViewById(R.id.desjejumButton);
         mAlmocoButton = (Button) findViewById(R.id.almocoButton);
         mJantarButton = (Button) findViewById(R.id.jantarButton);
+        mCardapioButton = (ImageButton) findViewById(R.id.cardapioButton);
+        mCredButton = (ImageButton) findViewById(R.id.creditoButton);
+        mInfoButton = (ImageButton) findViewById(R.id.infoButton);
+        mConfigButton = (ImageButton) findViewById(R.id.configButton);
+
+
+        View.OnClickListener listenerConfig = new View.OnClickListener() {  // Ao clicar no botao de configurações
+
+            public void onClick(View view) {
+                startActivityConfig(view);
+
+            }
+
+        };
+
+
+        View.OnClickListener listenerInfo = new View.OnClickListener() {  // Ao clicar no botao de informações
+
+            public void onClick(View view) {
+                startActivityInfo(view);
+
+            }
+
+        };
+
+
+        View.OnClickListener listenerCred = new View.OnClickListener() {  // Ao clicar no botao de créditos
+
+            public void onClick(View view) {
+                startActivityCredit(view);
+
+            }
+
+        };
+
+
+        View.OnClickListener listenerCard = new View.OnClickListener() {  // Ao clicar no botao de cardápio
+
+            public void onClick(View view) {
+                toast.show();
+
+            }
+
+        };
 
 
         View.OnClickListener listenerDesjejum = new View.OnClickListener() {  // Ao clicar no botao Desjejum
@@ -35,6 +92,7 @@ public class HomeActivity extends FragmentActivity {
 
         };
 
+
         View.OnClickListener listenerAlmoco = new View.OnClickListener() {  // Ao clicar no botao Almoco
 
             public void onClick(View view) {
@@ -42,6 +100,7 @@ public class HomeActivity extends FragmentActivity {
 
             }
         };
+
 
         View.OnClickListener listenerJantar = new View.OnClickListener() {  // Ao clicar no botao Jantar
 
@@ -51,24 +110,34 @@ public class HomeActivity extends FragmentActivity {
             }
         };
 
+
         mDesjejumButton.setOnClickListener(listenerDesjejum);
         mAlmocoButton.setOnClickListener(listenerAlmoco);
         mJantarButton.setOnClickListener(listenerJantar);
+        mCardapioButton.setOnClickListener(listenerCard);
+        mCredButton.setOnClickListener(listenerCred);
+        mConfigButton.setOnClickListener(listenerConfig);
+        mInfoButton.setOnClickListener(listenerInfo);
+
 
     }
+
 
     public void desjejumFragment(View view){
         replaceFragment(df);
 
     }
+
     public void almocoFragment(View view){
         replaceFragment(af);
 
     }
+
     public void jantarFragment(View view){
         replaceFragment(jf);
 
     }
+
 
     private void replaceFragment(Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -78,5 +147,25 @@ public class HomeActivity extends FragmentActivity {
 
         transaction.commit();
     }
+
+
+    public void startActivityCredit(View view) {
+
+        //Intent creditActivity = new Intent(this, HomeActivity.class);
+        //startActivity(creditActivity);
+    }
+
+    public void startActivityInfo(View view) {
+
+        //Intent homeActivity = new Intent(this, HomeActivity.class);
+        //startActivity(homeActivity);
+    }
+
+    public void startActivityConfig(View view) {
+
+        //Intent homeActivity = new Intent(this, HomeActivity.class);
+        //startActivity(homeActivity);
+    }
+
 
 }
